895bde893f25cf9732807e1af05ca26fd060e62a
MichelCode <michelseocode.com>
2023-11-14 15:41:23 +0100

    V8 Database

--- database/migrations/2023_11_14_140609_create_comments_table.php
+++ database/migrations/2023_11_14_140609_create_comments_table.php
@@ -11,8 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('posts', function (Blueprint $table) {
+        Schema::create('comments', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('post_id')->constrained()->onDelete('cascade');
+            $table->text('content');
             $table->timestamps();
         });
     }
@@ -22,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('posts');
+        Schema::dropIfExists('comments');
     }
 };
