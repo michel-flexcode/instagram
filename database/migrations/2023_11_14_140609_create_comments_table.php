e558ad554b2c7645e22274585ad6c9243d56c890
MichelCode <michelseocode.com>
2023-11-14 17:01:20 +0100

    V14 BUG factory corrigé

--- database/migrations/2023_11_14_140609_create_comments_table.php
+++ database/migrations/2023_11_14_140609_create_comments_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->foreignId('post_id')->constrained()->onDelete('cascade');
             $table->text('content');
+            $table->date('date');
             $table->timestamps();
         });
     }
