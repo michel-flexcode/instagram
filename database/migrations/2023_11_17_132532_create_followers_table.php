7c111705b0b19afc78dddcf1a6c28b917771ea7f
MichelCode <michelseocode.com>
2023-11-17 14:26:36 +0100

    v15 Added table follower

---
+++ database/migrations/2023_11_17_132532_create_followers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFollowersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('followers', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('follower_id'); // L'utilisateur qui suit
+            $table->unsignedBigInteger('user_id'); // L'utilisateur suivi
+            $table->timestamps();
+
+            $table->unique(['follower_id', 'user_id']); // Assure l'unicité des relations
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('followers');
+    }
+};
