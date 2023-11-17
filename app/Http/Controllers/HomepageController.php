509fbb463ba96feeb25e482ace3cc2bc3487bbbf
MichelCode <michelseocode.com>
2023-11-17 16:08:13 +0100

    v17 roads modifications

--- app/Http/Controllers/HomepageController.php
+++ app/Http/Controllers/HomepageController.php
@@ -3,8 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class HomepageController extends Controller
 {
-    //
+    public function index()
+    {
+        $posts = Post::all();
+
+        return view('homepage.index', [
+            'posts' => $posts,
+        ]);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomepageController::class, 'index']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -28,4 +26,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
