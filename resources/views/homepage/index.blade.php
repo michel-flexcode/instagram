bee92233722417d2dad537f6b651d2e4913c90b6
MichelCode <michelseocode.com>
2023-11-17 17:06:34 +0100

    v20 layout

--- resources/views/homepage/index.blade.php
+++ resources/views/homepage/index.blade.php
@@ -1,7 +1,11 @@
-<h1>Liste des articles</h1>
-<ul>
-    @foreach ($posts as $post)
-        <li>{{ $post->image_url }}</li>
-        <li>{{ $post->description }}</li>
-    @endforeach
-</ul>
+<x-guest-layout>
+    <h1 class="font-bold text-xl mb-4">Liste des articles</h1>
+    <ul class="grid sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-8">
+        <ul>
+            @foreach ($posts as $post)
+                <li>{{ $post->image_url }}</li>
+                <li>{{ $post->description }}</li>
+            @endforeach
+        </ul>
+    </ul>
+</x-guest-layout>
