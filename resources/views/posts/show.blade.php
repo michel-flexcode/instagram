03e314b60e62b805d4dc2f608d5a4e6c1eac6484
MichelCode <michelseocode.com>
2023-11-28 14:02:10 +0100

    v35 show

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,13 +1,19 @@
-@extends('layouts.app')
+<x-user-layout>
 
-@section('content')
-    <h1>{{ $post->title }}</h1>
-    <p>{{ $post->description }}</p>
+    <div class="max-w-2xl mx-auto mt-10 bg-white p-8 rounded-md shadow-md">
 
-    <!-- Add other post details as needed -->
 
-    <h2>Comments</h2>
-    @foreach ($descriptions as $description)
-        <p>{{ $description->content }} by {{ $description->user->name }}</p>
-    @endforeach
-@endsection
+        <div class="mb-4">
+            <img src="{{ $post->image_url }}" alt="{{ $post->description }}" class="w-full h-auto rounded-md">
+        </div>
+
+        <h1 class="text-2xl font-semibold mb-4">{{ $post->description }}</h1>
+        <p class="text-gray-700">{{ $post->user->name }}</p>
+        <p class="text-gray-500">{{ $post->created_at->diffForHumans() }}</p>
+
+        <div class="mt-8">
+            <a href="{{ route('posts.index') }}" class="text-blue-500 hover:underline">Back to Posts</a>
+        </div>
+    </div>
+
+</x-user-layout>
