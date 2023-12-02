a9b741be2db6d4e53fbeeccdb4cba5292f1c2180
MichelCode <michelseocode.com>
2023-12-02 14:23:57 +0100

    Navigation + posts v48

--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -13,51 +13,22 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body class="font-sans text-gray-900 antialiased">
+<body class="font-sans antialiased">
+    <div class="min-h-screen bg-gray-100">
+        @include('layouts.navigation')
 
-    <nav class="flex justify-between items-center py-4 px-8 bg-gray-100">
-        <div class="flex items-center space-x-4">
-            <a href="/"
-                class="group font-bold text-3xl flex items-center space-x-4 hover:text-emerald-600 transition">
-                <x-application-logo
-                    class="w-10 h-10 fill-current text-gray-500 group-hover:text-emerald-500 transition" />
-                <span>Instameme user.blade.php</span>
-            </a>
-        </div>
-
-        <div class="flex items-center space-x-4 cursor-pointer"
-            onclick="window.location.href='http://instameme.test/posts';">
-            <img src="https://placekitten.com/100/100" alt="Profile Picture" class="rounded-full w-10 h-10 mr-2">
-            <div class="flex items-center space-x-4">
-                <a href="{{ route('profile.update') }}"
-                    class="font-medium text-gray-600 hover:text-gray-500 transition">{{ Auth::user()->name }}user.blade.php</a>
-            </div>
-        </div>
-
-
-        <div class="flex items-center space-x-4 cursor-pointer">
-            <a href="http://instameme.test/posts/create" class="flex items-center space-x-4">
-                <img src="https://placekitten.com/100/100" alt="Profile Picture" class="rounded-full w-10 h-10 mr-2">
-                <div class="flex items-center space-x-4">
-                    <a href="{{ route('profile.update') }}"
-                        class="font-medium text-gray-600 hover:text-gray-500 transition">{{ Auth::user()->name }}user.blade.php</a>
+        <!-- Page Heading -->
+        @if (isset($header))
+            <header class="bg-white shadow">
+                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                    {{ $header }}
                 </div>
-            </a>
-
-            <div>
-                <form action="{{ route('logout') }}" method="post">
-                    @csrf
-                    <button type="submit"
-                        class="font-medium text-gray-600 hover:text-gray-500 transition">Déconnexion</button>
-                </form>
-            </div>
-        </div>
-    </nav>
-
-    {{-- Sert ici à foutre les posts --}}
-    <main>
-        {{ $slot }}
-    </main>
+            </header>
+        @endif
+        {{-- Sert ici à foutre les posts --}}
+        <main>
+            {{ $slot }}
+        </main>
 
     </div>
     </div>
