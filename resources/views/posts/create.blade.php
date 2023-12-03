9efe328776850d929104d083aaf91aea9de09542
MichelCode <michelseocode.com>
2023-12-03 10:31:52 +0100

    v64 propre

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,57 +1,59 @@
 <x-user-layout>
+ <div class="bg-gradient-to-b from-blue-500 to-purple-700">
 
-    <x-slot name="header">
-        <h2 class="text-4xl font-bold mb-6">
-            {{ __('Posts') }}
-        </h2>
-    </x-slot>
+        <x-slot name="header">
+            <h2 class="text-4xl font-bold mb-6">
+                {{ __('Posts') }}
+            </h2>
+        </x-slot>
 
-    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 py-12">
-        <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-6">
-            <div class="flex justify-between mt-8">
-                <div class="text-2xl">
-                    Post you InstaMeme !
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 py-12">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-6">
+                <div class="flex justify-between mt-8">
+                    <div class="text-2xl">
+                        Post you InstaMeme !
+                    </div>
                 </div>
+
+                <form method="POST" action="{{ route('posts.store') }}" class="flex flex-col space-y-4 text-gray-500"
+                    enctype="multipart/form-data">
+
+                    @csrf
+
+                    <div>
+                        <x-input-label for="description" :value="__('Description')" />
+                        <x-text-input id="description" class="block mt-1 w-full" type="text" name="description"
+                            :value="old('description')" autofocus />
+                        <x-input-error :messages="$errors->get('description')" class="mt-2" />
+                    </div>
+
+                    <div>
+                        <x-input-label for="localisation" :value="__('Localisation')" />
+                        <x-text-input id="localisation" class="block mt-1 w-full" type="text" name="localisation"
+                            :value="old('localisation')" />
+                        <x-input-error :messages="$errors->get('localisation')" class="mt-2" />
+                    </div>
+
+                    <div>
+                        <x-input-label for="date" :value="__('Date de publication')" />
+                        <x-text-input id="date" class="block mt-1 w-full" type="date" name="date"
+                            :value="old('date')" />
+                        <x-input-error :messages="$errors->get('date')" class="mt-2" />
+                    </div>
+
+                    <div>
+                        <x-input-label for="image" :value="__('Image')" />
+                        <x-text-input id="image" class="block mt-1 w-full" type="file" name="image" />
+                        <x-input-error :messages="$errors->get('image')" class="mt-2" />
+                    </div>
+
+                    <div class="flex justify-end">
+                        <x-primary-button type="submit">
+                            {{ __('Créer') }}
+                        </x-primary-button>
+                    </div>
+                </form>
             </div>
-
-            <form method="POST" action="{{ route('posts.store') }}" class="flex flex-col space-y-4 text-gray-500"
-                enctype="multipart/form-data">
-
-                @csrf
-
-                <div>
-                    <x-input-label for="description" :value="__('Description')" />
-                    <x-text-input id="description" class="block mt-1 w-full" type="text" name="description"
-                        :value="old('description')" autofocus />
-                    <x-input-error :messages="$errors->get('description')" class="mt-2" />
-                </div>
-
-                <div>
-                    <x-input-label for="localisation" :value="__('Localisation')" />
-                    <x-text-input id="localisation" class="block mt-1 w-full" type="text" name="localisation"
-                        :value="old('localisation')" />
-                    <x-input-error :messages="$errors->get('localisation')" class="mt-2" />
-                </div>
-
-                <div>
-                    <x-input-label for="date" :value="__('Date de publication')" />
-                    <x-text-input id="date" class="block mt-1 w-full" type="date" name="date"
-                        :value="old('date')" />
-                    <x-input-error :messages="$errors->get('date')" class="mt-2" />
-                </div>
-
-                <div>
-                    <x-input-label for="image" :value="__('Image')" />
-                    <x-text-input id="image" class="block mt-1 w-full" type="file" name="image" />
-                    <x-input-error :messages="$errors->get('image')" class="mt-2" />
-                </div>
-
-                <div class="flex justify-end">
-                    <x-primary-button type="submit">
-                        {{ __('Créer') }}
-                    </x-primary-button>
-                </div>
-            </form>
         </div>
     </div>
 </x-user-layout>
